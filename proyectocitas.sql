39b03ba2b502092a2ab9c4550e49580de84f3b2e
Dario <darioleong1997@gmail.com>
2020-10-23 21:28:28 +0200

    Actualizada BBDD

--- proyectocitas.sql
+++ proyectocitas.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 23-10-2020 a las 13:35:13
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+-- Servidor: localhost:3306
+-- Tiempo de generación: 23-10-2020 a las 21:27:45
+-- Versión del servidor: 8.0.21-0ubuntu0.20.04.4
+-- Versión de PHP: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -28,17 +29,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `amigos` (
-  `emailUsu-1` varchar(50) NOT NULL,
-  `emailUsu-2` varchar(50) NOT NULL,
-  `acepta` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `amigos`
---
-
-INSERT INTO `amigos` (`emailUsu-1`, `emailUsu-2`, `acepta`) VALUES
-('darioleong@gmail.com', 'otroej@gmail.com', 0);
+  `usu1` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `usu2` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `acepta` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -47,25 +41,15 @@ INSERT INTO `amigos` (`emailUsu-1`, `emailUsu-2`, `acepta`) VALUES
 --
 
 CREATE TABLE `mensaje` (
-  `idMail` int(1) NOT NULL,
+  `idMail` int NOT NULL,
   `asunto` varchar(50) NOT NULL,
   `cuerpo` varchar(5000) NOT NULL,
   `emisor` varchar(50) NOT NULL,
   `receptor` varchar(50) NOT NULL,
   `fecha` date NOT NULL,
-  `leido` tinyint(1) NOT NULL DEFAULT 0,
+  `leido` tinyint(1) NOT NULL DEFAULT '0',
   `fileName` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `mensaje`
---
-
-INSERT INTO `mensaje` (`idMail`, `asunto`, `cuerpo`, `emisor`, `receptor`, `fecha`, `leido`, `fileName`) VALUES
-(1, 'Prueba', 'Esto es un mensaje de prueba', 'darioleong@gmail.com', 'otroej@gmail.com', '2020-10-21', 0, ''),
-(11, 'Prueba', 'Esto es un mensaje de prueba', 'darioleong@gmail.com', 'otroej@gmail.com', '2020-10-21', 0, ''),
-(12, 'tertte', 'werwer', 'darioleong@gmail.com', 'otroej@gmail.com', '2020-10-21', 0, 'github.txt'),
-(13, 'asdasda', 'qweqweqw', 'darioleong@gmail.com', 'otroej@gmail.com', '2020-10-21', 0, 'proyectocitas.sql');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -76,20 +60,20 @@ INSERT INTO `mensaje` (`idMail`, `asunto`, `cuerpo`, `emisor`, `receptor`, `fech
 CREATE TABLE `preferencias` (
   `emailUsu` varchar(50) NOT NULL,
   `relacion` tinyint(1) NOT NULL,
-  `deportes` int(11) NOT NULL,
-  `arte` int(11) NOT NULL,
-  `politica` int(11) NOT NULL,
+  `deportes` int NOT NULL,
+  `arte` int NOT NULL,
+  `politica` int NOT NULL,
   `hijos` tinyint(1) NOT NULL,
   `interes` char(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `preferencias`
 --
 
 INSERT INTO `preferencias` (`emailUsu`, `relacion`, `deportes`, `arte`, `politica`, `hijos`, `interes`) VALUES
-('darioleong@gmail.com', 1, 50, 50, 50, 0, 'f'),
-('otroej@gmail.com', 1, 100, 90, 0, 0, 'b');
+('darioleong@gmail.com', 1, 66, 78, 23, 0, 'f'),
+('otroej@gmail.com', 1, 20, 100, 20, 0, 'b');
 
 -- --------------------------------------------------------
 
@@ -98,9 +82,9 @@ INSERT INTO `preferencias` (`emailUsu`, `relacion`, `deportes`, `arte`, `politic
 --
 
 CREATE TABLE `roles` (
-  `idRol` int(11) NOT NULL,
+  `idRol` int NOT NULL,
   `descripcion` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `roles`
@@ -117,19 +101,18 @@ INSERT INTO `roles` (`idRol`, `descripcion`) VALUES
 --
 
 CREATE TABLE `rolusuario` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `emailUsu` varchar(50) NOT NULL,
-  `idRol` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `idRol` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `rolusuario`
 --
 
 INSERT INTO `rolusuario` (`id`, `emailUsu`, `idRol`) VALUES
-(1, 'darioleong@gmail.com', 0),
-(2, 'darioleong@gmail.com', 1),
-(4, 'otroej@gmail.com', 1);
+(9, 'darioleong@gmail.com', 0),
+(10, 'otroej@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -144,16 +127,16 @@ CREATE TABLE `usuario` (
   `apellido` varchar(50) NOT NULL,
   `edad` varchar(50) NOT NULL,
   `genero` char(1) NOT NULL,
-  `activo` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `activo` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
 INSERT INTO `usuario` (`email`, `pass`, `nombre`, `apellido`, `edad`, `genero`, `activo`) VALUES
-('darioleong@gmail.com', '1', 'Darío', 'León', '23', 'm', 1),
-('otroej@gmail.com', '3', 'Sara', 'Ramirez', '30', 'f', 1);
+('darioleong@gmail.com', 'Ab123456', 'Dario', 'Leon', '23', 'm', 1),
+('otroej@gmail.com', '123456Ab', 'Sara', 'Ramirez', '30', 'f', 0);
 
 --
 -- Índices para tablas volcadas
@@ -163,8 +146,8 @@ INSERT INTO `usuario` (`email`, `pass`, `nombre`, `apellido`, `edad`, `genero`, 
 -- Indices de la tabla `amigos`
 --
 ALTER TABLE `amigos`
-  ADD PRIMARY KEY (`emailUsu-1`,`emailUsu-2`),
-  ADD KEY `email2` (`emailUsu-2`);
+  ADD PRIMARY KEY (`usu1`,`usu2`),
+  ADD KEY `email2` (`usu2`);
 
 --
 -- Indices de la tabla `mensaje`
@@ -206,13 +189,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `mensaje`
 --
 ALTER TABLE `mensaje`
-  MODIFY `idMail` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `idMail` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `rolusuario`
 --
 ALTER TABLE `rolusuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
@@ -222,8 +205,8 @@ ALTER TABLE `rolusuario`
 -- Filtros para la tabla `amigos`
 --
 ALTER TABLE `amigos`
-  ADD CONSTRAINT `email1` FOREIGN KEY (`emailUsu-1`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `email2` FOREIGN KEY (`emailUsu-2`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `email1` FOREIGN KEY (`usu1`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `email2` FOREIGN KEY (`usu2`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `preferencias`
